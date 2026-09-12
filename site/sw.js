9edc1cd4b18585b3a879fb2e70f8119a4c1a1aa8
abhishekand1111-prog <abhishekand.1111@gmail.com>
2026-09-12 17:02:17 +0530

    Initial commit

---
+++ site/sw.js
@@ -0,0 +1,32 @@
+const CACHE = 'diagnosis-room-v1';
+const SHELL = ['./', './index.html', './manifest.json', './icons/icon-192.png', './icons/icon-512.png'];
+
+self.addEventListener('install', function(event){
+  event.waitUntil(caches.open(CACHE).then(function(cache){ return cache.addAll(SHELL); }));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(event){
+  event.waitUntil(
+    caches.keys().then(function(keys){
+      return Promise.all(keys.filter(function(k){ return k !== CACHE; }).map(function(k){ return caches.delete(k); }));
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', function(event){
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    caches.match(event.request).then(function(cached){
+      var fetchPromise = fetch(event.request).then(function(networkResponse){
+        if (networkResponse && networkResponse.ok){
+          var copy = networkResponse.clone();
+          caches.open(CACHE).then(function(cache){ cache.put(event.request, copy); });
+        }
+        return networkResponse;
+      }).catch(function(){ return cached; });
+      return cached || fetchPromise;
+    })
+  );
+});
