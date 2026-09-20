06fed6d42dd03c8972b457a34ac1b5594eb6f6f8
abhishekand1111-prog <abhishekand.1111@gmail.com>
2026-09-20 11:54:44 +0530

    Icon
    icon

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'diagnosis-room-v2';
+﻿const CACHE = 'diagnosis-room-v3';
 const SHELL = ['./', './index.html', './manifest.json', './icons/icon-192.png', './icons/icon-512.png'];
 
 self.addEventListener('install', function(event){
