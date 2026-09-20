2f52993d77b5ff14cec9cd7a0766ae14a1fc015d
abhishekand1111-prog <abhishekand.1111@gmail.com>
2026-09-20 12:59:59 +0530

    Price changes

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'diagnosis-room-v6';
+const CACHE = 'diagnosis-room-v7';
 const SHELL = ['./', './index.html', './manifest.json', './icons/icon-192.png', './icons/icon-512.png', './icons/apple-touch-icon.png', './icons/favicon-32.png'];
 
 self.addEventListener('install', function(event){
