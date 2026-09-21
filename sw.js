9b523365cd8735a89d860e357cbe397c1849e403
abhishekand1111-prog <abhishekand.1111@gmail.com>
2026-09-21 09:18:23 +0530

    tasj

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'diagnosis-room-v20';
+const CACHE = 'diagnosis-room-v21';
 const SHELL = ['./', './index.html', './manifest.json', './icons/icon-192.png', './icons/icon-512.png', './icons/apple-touch-icon.png', './icons/favicon-32.png'];
 
 self.addEventListener('install', function(event){
