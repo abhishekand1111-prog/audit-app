80fe396c5f66953fc7b8f5d52b89f6e769afd3e8
abhishekand1111-prog <abhishekand.1111@gmail.com>
2026-09-25 17:36:27 +0530

    landing page
    landing page and app data combination

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'diagnosis-room-v37';
+const CACHE = 'diagnosis-room-v40';
 const SHELL = ['./', './index.html', './manifest.json', './icons/icon-192.png', './icons/icon-512.png', './icons/apple-touch-icon.png', './icons/favicon-32.png'];
 
 self.addEventListener('install', function(event){
