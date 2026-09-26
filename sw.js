e626aad183d1978d99931487c11cd50467fb0512
abhishekand1111-prog <abhishekand.1111@gmail.com>
2026-09-26 21:17:44 +0530

    dashboard changes done

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'diagnosis-room-v44';
+const CACHE = 'diagnosis-room-v46';
 const SHELL = ['./', './index.html', './manifest.json', './icons/icon-192.png', './icons/icon-512.png', './icons/apple-touch-icon.png', './icons/favicon-32.png'];
 
 self.addEventListener('install', function(event){
